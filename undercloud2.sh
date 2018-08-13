43f0fecf747e4f2d81899f86a51d984b9a764474
ashince <ashincekd@gmail.com>
2018-08-13 23:58:19 +0530

    Update undercloud2.sh

--- undercloud2.sh
+++ undercloud2.sh
@@ -3,6 +3,6 @@ ip a
 cat /etc/sysconfig/network-scripts/ifcfg-eth0
 ping -c1 www.redhat.com
 useradd stack
- mkdir /home/stack/.ssh
- cp /root/.ssh/authorized_keys /home/stack/.ssh/
- chown -R stack:stack /home/stack/.ssh
+mkdir /home/stack/.ssh
+cp /root/.ssh/authorized_keys /home/stack/.ssh/
+chown -R stack:stack /home/stack/.ssh
