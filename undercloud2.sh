95d71bd7305db9d686228e23ecf82a4052a209fd
ashince <ashincekd@gmail.com>
2018-08-13 23:57:41 +0530

    Create undercloud2.sh

---
+++ undercloud2.sh
@@ -0,0 +1,8 @@
+hostnamectl --static status
+ip a
+cat /etc/sysconfig/network-scripts/ifcfg-eth0
+ping -c1 www.redhat.com
+useradd stack
+ mkdir /home/stack/.ssh
+ cp /root/.ssh/authorized_keys /home/stack/.ssh/
+ chown -R stack:stack /home/stack/.ssh
