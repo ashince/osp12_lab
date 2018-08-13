416fa4ea9c11752b2dbc5cf909285ca329a9e50a
ashince <ashincekd@gmail.com>
2018-08-14 00:05:46 +0530

    Update undercloud4.sh

--- undercloud4.sh
+++ undercloud4.sh
@@ -1,2 +1,4 @@
-cp undercloud.conf /home/stack/undercloud.conf
-echo "You can start the undercloud installation via \"openstack undercloud install\""
+cp undercloud.conf /home/stack/undercloud.conf\
+echo "1. Switch to stack user via cmd \"su - stack\""
+echo "2. Check if you are in Home directory of stack user"
+echo "3. Start Undercloud installation via \"openstack undercloud install\""
